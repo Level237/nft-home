0eae5a0d5f4616e75715e9df3aa5c31ee0d1841d
level237 <Bramslevel129@gmail.com>
2024-03-18 00:04:26 +0100

    init scroll

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,16 +2,98 @@
 
 import React from 'react'
 import Image from 'next/image';
+import logo from "../../../public/logo.png";
+import styled from "styled-components";
+import Button from '../button/Button';
 
 function Header() {
   return (
-    <div>
-     <nav className='logo'>
-       
+    <HeaderStyled>
+     <nav>
+        <div className="logo">
+        <Image src={logo} alt='logo' width={36}/>
+        <h2>Logo</h2>
+        </div>
+        <div className="input">
+            <input type="text" placeholder='Search'/>
+        </div>
+       <ul className="nav-items">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Auctions</a>
+
+        </li>
+        <li>
+            <a href="">Marketplace</a>
+            
+        </li>
+        <li>
+            <a href="">About</a>
+            
+        </li>
+        <li>
+            <Button/>
+            
+        </li>
+       </ul>
      </nav>
-    </div>
+    </HeaderStyled>
   )
 }
+
+const HeaderStyled=styled.header`
+
+nav{
+    padding:0.4rem;
+    min-height:10vh;
+    border-bottom:1px solid var(--color-border);
+    display:flex;
+    justify-content:space-between;
+    align-items:center; color: hsla(180, 7%,75%,0.9);
+
+    .logo{
+        display:flex;
+        align-items:center;
+        gap:1rem;
+        cursor:pointer
+    }
+
+    .input{
+        flex:2;
+        display:flex;
+        justify-content:center;
+
+        input{
+            width:55%;
+            padding:0.6rem 0.8rem;
+            border-radius:8px;
+            background-color:#161616;
+            border:1px solid var(--color-border);
+            &::placeholder{
+                color:var(--color-border);
+                font-weight:500
+            }
+        }
+    }
+
+    .nav-items{
+        display:flex;
+        align-items:center;
+        gap:2rem;
+
+        li{
+            transition:all 0.2s ease-in-out;
+
+            &:hover{
+                color:white;
+                transform:scale(1.1)
+            }
+        }
+    }
+}
+`;
 
 export default Header
 
---
+++ app/components/button/Button.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+function Button() {
+  return (
+    <div>
+      <button></button>
+    </div>
+  )
+}
+
+export default Button
