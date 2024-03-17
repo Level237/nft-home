3bde710509fb617e05fe437192d4cd781887cff0
level237 <Bramslevel129@gmail.com>
2024-03-17 23:12:36 +0100

    init scroll

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react'
+import Image from 'next/image';
+
+function Header() {
+  return (
+    <div>
+     <nav className='logo'>
+       
+     </nav>
+    </div>
+  )
+}
+
+export default Header
+
